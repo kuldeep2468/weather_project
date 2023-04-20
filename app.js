401c8a1f8b10e3a7508b08151d66875a89ed06bc
kuldeepbhatrigoswami <cooldeep.go@gmail.com>
2023-04-20 13:22:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const https = require("https");
+const apiKey = '3e6b0feff864b38c68e52f462a3c1eef'
+const app= express();
+
+app.get("/", function(req, res){
+    const url = ("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=3e6b0feff864b38c68e52f462a3c1eef")
+https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const weatherDescription = weatherData.weather[0].description
+        console.log(temp);
+    })
+})
+    res.send("Server is up and running");
+})
+
+app.listen (3000, function(){
+    console.log("Server is on running on port 3000");
+})
